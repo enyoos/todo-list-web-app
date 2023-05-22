8976740c0666e1f78cd2e88d51e13596f97d703d
enyoos <lebedevhh@outlook.fr>
2023-05-21 21:52:56 -0400

    *

---
+++ src/main/java/com/todo/App.java
@@ -0,0 +1,14 @@
+package com.todo;
+
+import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
